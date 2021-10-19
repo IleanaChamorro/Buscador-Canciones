78c796a92e5c2f9134ac4d0c797582cc65831bde
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-10-19 10:12:19 -0300

    Peticiones Ajax a Api's

--- src/componentes/BuscadorCanciones.js
+++ src/componentes/BuscadorCanciones.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { helpHttp } from '../helper/helpHttp';
 import CancionDetalles from './CancionDetalles';
 import CancionForm from './CancionForm';
 import Loader from './Loader';
@@ -9,12 +10,43 @@ const BuscadorCanciones = () => {
     const [bio, setBio] = useState(null);
     const [cargador, setCargador] = useState(false);
 
-    //Manejador evento submit form
+    useEffect(() => {
+        //Mientras empieza la busqueda, se sale del renderizado
+        if(buscador === null) return;
 
+        const fetchData = async () => {
+            //Destructuracion propiedades de buscador
+            const {artista, cancion} = buscador;
+         
+            //Obtencion Informacion de Artista y Cancion
+            let artistaUrl =  `theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+            let cancionUrl = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+        
+            console.log(artistaUrl, cancionUrl);
+
+            //Loader espera a respuesta peticion 
+            setCargador(true);
+
+            const [artistaRes, cancionRes] = await Promise.all([
+                helpHttp().get(artistaUrl),
+                helpHttp().get(cancionUrl),
+            ]);
+
+            console.log(artistaRes, cancionRes);
+
+            setBio(artistaRes);
+            setLetra(cancionRes);
+            setCargador(false);
+        };
+        fetchData();
+    }, [buscador]);
+    
+    
+    //Manejador evento submit form
     const handleSearch = (data) => {
         //console.log(data);
         setBuscador(data);
-    }
+    };
     return (
         <div>
             <h2>Buscador canciones</h2>
